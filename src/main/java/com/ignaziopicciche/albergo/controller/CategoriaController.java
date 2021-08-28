9de8221919f8689e6a3fdff7180b4ba10648d08b
Ignazio Emanuele Piccichè <ignazioemanuele.picciche@gmail.com>
2021-08-28 13:15:36 +0200

    Implementazioine controllerCategoria

---
+++ src/main/java/com/ignaziopicciche/albergo/controller/CategoriaController.java
@@ -0,0 +1,15 @@
+package com.ignaziopicciche.albergo.controller;
+
+import com.ignaziopicciche.albergo.service.CategoriaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/categoria")
+public class CategoriaController {
+
+    @Autowired
+    private CategoriaService
+
+}
