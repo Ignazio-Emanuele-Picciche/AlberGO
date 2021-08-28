26ef4df19f13c9be570f40af85ae670fa0b329d3
Ignazio Emanuele Piccichè <ignazioemanuele.picciche@gmail.com>
2021-08-28 11:07:21 +0200

    prova

---
+++ src/main/java/com/ignaziopicciche/albergo/AlberGoApplication.java
@@ -0,0 +1,13 @@
+package com.ignaziopicciche.albergo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlberGoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlberGoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ignaziopicciche/albergo/AlberGoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ignaziopicciche.albergo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlberGoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
