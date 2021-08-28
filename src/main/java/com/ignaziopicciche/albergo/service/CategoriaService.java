fdf43d81910e3b83804ed3244324b68f49aed3ce
Ignazio Emanuele Piccichè <ignazioemanuele.picciche@gmail.com>
2021-08-28 13:28:36 +0200

    Implementazioine CategoriaService

--- src/main/java/com/ignaziopicciche/albergo/service/CategoriaService.java
+++ src/main/java/com/ignaziopicciche/albergo/service/CategoriaService.java
@@ -5,9 +5,12 @@ import com.ignaziopicciche.albergo.dto.CategoriaDTO;
 import com.ignaziopicciche.albergo.helper.CategoriaHelper;
 import com.ignaziopicciche.albergo.model.Categoria;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.parameters.P;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Objects;
+import java.util.Optional;
 
 @Service
 public class CategoriaService {
@@ -22,6 +25,36 @@ public class CategoriaService {
         Preconditions.checkArgument(!Objects.isNull(idHotel));
 
         return categoriaHelper.create(categoriaDTO, idHotel);
+    }
+
+
+    public boolean delete(Long id){
+        Preconditions.checkArgument(!Objects.isNull(id));
+
+        return categoriaHelper.delete(id);
+    }
+
+
+    public Optional<Categoria> findById(Long id){
+        Preconditions.checkArgument(!Objects.isNull(id));
+
+        return categoriaHelper.findById(id);
+    }
+
+    public Categoria update(CategoriaDTO categoriaDTO){
+        Preconditions.checkArgument(!Objects.isNull(categoriaDTO.id));
+        Preconditions.checkArgument(!Objects.isNull(categoriaDTO.nome));
+        Preconditions.checkArgument(!Objects.isNull(categoriaDTO.descrizione));
+        Preconditions.checkArgument(!Objects.isNull(categoriaDTO.prezzo));
+
+        return categoriaHelper.update(categoriaDTO);
+    }
+
+
+    public List<Categoria> findAll(Long idHotel){
+        Preconditions.checkArgument(!Objects.isNull(idHotel));
+
+        return categoriaHelper.findAll(idHotel);
     }
 
 }
