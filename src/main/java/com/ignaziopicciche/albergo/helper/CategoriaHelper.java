a0421faa1a17343f45daf7d51632807920f00682
Ignazio Emanuele Piccichè <ignazioemanuele.picciche@gmail.com>
2021-08-28 12:56:16 +0200

    Implementazione Categoriahelper e query

--- src/main/java/com/ignaziopicciche/albergo/helper/CategoriaHelper.java
+++ src/main/java/com/ignaziopicciche/albergo/helper/CategoriaHelper.java
@@ -2,6 +2,7 @@ package com.ignaziopicciche.albergo.helper;
 
 import com.ignaziopicciche.albergo.dto.CategoriaDTO;
 import com.ignaziopicciche.albergo.exception.CategoriaException;
+import com.ignaziopicciche.albergo.exception.HotelException;
 import com.ignaziopicciche.albergo.model.Categoria;
 import com.ignaziopicciche.albergo.model.Hotel;
 import com.ignaziopicciche.albergo.repository.CategoriaRepository;
@@ -9,6 +10,7 @@ import com.ignaziopicciche.albergo.repository.HotelRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
 import java.util.Optional;
 
 @Component
@@ -20,10 +22,10 @@ public class CategoriaHelper {
     @Autowired
     private HotelRepository hotelRepository;
 
-    public Categoria create(CategoriaDTO categoriaDTO){
+    public Categoria create(CategoriaDTO categoriaDTO) {
 
-        if(categoriaRepository.existsByNameAndIdHotel(categoriaDTO.nome, categoriaDTO.idHotel) &&
-                !!categoriaDTO.nome.equals("") && !!categoriaDTO.descrizione.equals("")){
+        if (categoriaRepository.existsByNameAndIdHotel(categoriaDTO.nome, categoriaDTO.idHotel) &&
+                !!categoriaDTO.nome.equals("") && !!categoriaDTO.descrizione.equals("")) {
 
             Categoria categoria = new Categoria();
             Optional<Hotel> hotel = Optional.of(new Hotel());
@@ -41,4 +43,68 @@ public class CategoriaHelper {
         throw new CategoriaException(CategoriaException.CategoriaExcpetionCode.CATEGORIA_ALREADY_EXISTS);
 
     }
+
+    public Boolean delete(Long id) {
+
+        if (categoriaRepository.existsById(id)) {
+            try {
+                categoriaRepository.deleteById(id);
+                return true;
+            } catch (Exception e) {
+                throw new CategoriaException(CategoriaException.CategoriaExcpetionCode.CATEGORIA_DELETE_ERROR);
+            }
+        }
+
+        throw new CategoriaException(CategoriaException.CategoriaExcpetionCode.CATEGORIA_ID_NOT_EXIST);
+    }
+
+
+    public Optional<Categoria> findById(Long id){
+        if(categoriaRepository.existsById(id)){
+            return categoriaRepository.findById(id);
+        }
+
+        throw new CategoriaException(CategoriaException.CategoriaExcpetionCode.CATEGORIA_NOT_FOUND);
+    }
+
+
+    public Categoria update(CategoriaDTO categoriaDTO){
+
+        if(categoriaRepository.existsById(categoriaDTO.id)){
+            Categoria categoria = new Categoria();
+            categoria.setId(categoriaDTO.id);
+            categoria.setNome(categoriaDTO.nome);
+            categoria.setDescrizione(categoriaDTO.descrizione);
+            categoria.setPrezzo(categoriaDTO.prezzo);
+
+            return categoriaRepository.save(categoria);
+        }
+
+        throw new CategoriaException(CategoriaException.CategoriaExcpetionCode.CATEGORIA_ID_NOT_EXIST);
+
+    }
+
+
+    public List<Categoria> findAll(Long idHotel){
+
+        if(hotelRepository.existsById(idHotel)){
+            return categoriaRepository.findAllByIdHotel(idHotel);
+        }
+
+        throw new HotelException(HotelException.HotelExceptionCode.HOTEL_ID_NOT_EXIST);
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
--- src/main/java/com/ignaziopicciche/albergo/repository/CategoriaRepository.java
+++ src/main/java/com/ignaziopicciche/albergo/repository/CategoriaRepository.java
@@ -5,9 +5,15 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.util.List;
+
 public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
 
     @Query("Select c from Categoria c where c.nome = :n and c.hotel = :h")
     Boolean existsByNameAndIdHotel(@Param("n") String name, @Param("h") Long idHotel);
+
+
+    @Query("select c from Categoria c where c.hotel = :h order by c.id asc")
+    List<Categoria> findAllByIdHotel(@Param("h") Long idHotel);
 
 }
