118461cdad18d0e8ce0f584f576f4c8bf1b90856
Ignazio Emanuele Piccichè <ignazioemanuele.picciche@gmail.com>
2021-09-03 21:36:03 +0200

    Impl PrenotazionHelper

--- src/main/java/com/ignaziopicciche/albergo/helper/PrenotazioneHelper.java
+++ src/main/java/com/ignaziopicciche/albergo/helper/PrenotazioneHelper.java
@@ -1,9 +1,12 @@
 package com.ignaziopicciche.albergo.helper;
 
+import com.ignaziopicciche.albergo.dto.PrenotazioneClienteStanzaDTO;
 import com.ignaziopicciche.albergo.dto.PrenotazioneDTO;
 import com.ignaziopicciche.albergo.exception.HotelException;
 import com.ignaziopicciche.albergo.exception.PrenotazioneException;
+import com.ignaziopicciche.albergo.model.Cliente;
 import com.ignaziopicciche.albergo.model.Prenotazione;
+import com.ignaziopicciche.albergo.model.Stanza;
 import com.ignaziopicciche.albergo.repository.ClienteRepository;
 import com.ignaziopicciche.albergo.repository.HotelRepository;
 import com.ignaziopicciche.albergo.repository.PrenotazioneRepository;
@@ -11,6 +14,7 @@ import com.ignaziopicciche.albergo.repository.StanzaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -38,17 +42,56 @@ public class PrenotazioneHelper {
         throw new PrenotazioneException(PrenotazioneException.PrenotazioneExceptionCode.PRENOTAZIONE_ID_NOT_EXIST);
     }
 
-    public List<PrenotazioneDTO> findAll(Long idHotel){
+    public List<PrenotazioneClienteStanzaDTO> findAll(Long idHotel){
         if(hotelRepository.existsById(idHotel)){
-            return prenotazioneRepository.findPrenotazioneByHotel_Id(idHotel).stream().map(x -> new PrenotazioneDTO(x)).collect(Collectors.toList());
+           List<Prenotazione> prenotazioni = prenotazioneRepository.findPrenotazionesByHotel_Id(idHotel);
+           List<PrenotazioneClienteStanzaDTO> prenotazioniList = new ArrayList<>();
+
+           for(Prenotazione p: prenotazioni){
+               Cliente c = clienteRepository.findById(p.getCliente().getId()).get();
+               Stanza s = stanzaRepository.findById(p.getStanza().getId()).get();
+               prenotazioniList.add(new PrenotazioneClienteStanzaDTO(p, c, s));
+           }
+
+           return prenotazioniList;
         }
 
         throw new HotelException(HotelException.HotelExceptionCode.HOTEL_ID_NOT_EXIST);
     }
 
 
-    public PrenotazioneDTO create(PrenotazioneDTO prenotazioneDTO){
-        if(!prenotazioneRepository.existsById(prenotazioneDTO.id) &&  )
+    public Boolean delete(Long id){
+        if(prenotazioneRepository.existsById(id)){
+            try{
+                prenotazioneRepository.deleteById(id);
+                return true;
+            }catch (Exception e){
+                throw new PrenotazioneException(PrenotazioneException.PrenotazioneExceptionCode.PRENOTAZIONE_DELETE_ERROR);
+            }
+        }
+
+        throw new PrenotazioneException(PrenotazioneException.PrenotazioneExceptionCode.PRENOTAZIONE_NOT_FOUND);
     }
+
+
+    public PrenotazioneDTO create(PrenotazioneDTO prenotazioneDTO){
+        if(!prenotazioneRepository.existsById(prenotazioneDTO.id)){
+
+            Prenotazione p = new Prenotazione();
+            p.setDataInizio(prenotazioneDTO.dataInizio);
+            p.setDataFine(prenotazioneDTO.dataFine);
+            p.setCliente(clienteRepository.findById(prenotazioneDTO.idCliente).get());
+            p.setStanza(stanzaRepository.findById(prenotazioneDTO.idStanza).get());
+            p.setHotel(hotelRepository.findById(prenotazioneDTO.idHotel).get());
+
+            prenotazioneRepository.save(p);
+            return new PrenotazioneDTO(p);
+        }
+
+        throw new PrenotazioneException(PrenotazioneException.PrenotazioneExceptionCode.PRENOTAZIONE_ALREADY_EXISTS);
+    }
+
+
+
 
 }
--- src/main/java/com/ignaziopicciche/albergo/repository/PrenotazioneRepository.java
+++ src/main/java/com/ignaziopicciche/albergo/repository/PrenotazioneRepository.java
@@ -2,11 +2,21 @@ package com.ignaziopicciche.albergo.repository;
 
 import com.ignaziopicciche.albergo.model.Prenotazione;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.nio.channels.SelectableChannel;
 import java.util.List;
 
 public interface PrenotazioneRepository extends JpaRepository<Prenotazione, Long> {
 
-    List<Prenotazione> findPrenotazioneByHotel_Id(Long idHotel);
+    List<Prenotazione> findPrenotazionesByHotel_Id(Long idHotel);
+
+    @Query("select p from Prenotazione p where p.dataInizio <= CURRENT_DATE and p.dataFine >= CURRENT_DATE " +
+            "and p.id = :idPrenotazione")
+    Boolean checkDataPrenotazione(Long idPrenotazione);
+
+
+    @Query("")
+    Prenotazione checkStanzaPrenotazione(Long idStanza, Long idPrenotaione);
 
 }
