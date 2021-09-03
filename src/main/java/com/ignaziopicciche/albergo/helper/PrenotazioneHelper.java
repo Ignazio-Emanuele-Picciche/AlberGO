95c386b8d69d406cc5b7574268e5e15508e17097
Ignazio Emanuele Piccichè <ignazioemanuele.picciche@gmail.com>
2021-09-03 19:26:45 +0200

    Impl prenotazioneClienteStanza DTO

--- src/main/java/com/ignaziopicciche/albergo/helper/PrenotazioneHelper.java
+++ src/main/java/com/ignaziopicciche/albergo/helper/PrenotazioneHelper.java
@@ -1,12 +1,18 @@
 package com.ignaziopicciche.albergo.helper;
 
 import com.ignaziopicciche.albergo.dto.PrenotazioneDTO;
+import com.ignaziopicciche.albergo.exception.HotelException;
+import com.ignaziopicciche.albergo.exception.PrenotazioneException;
+import com.ignaziopicciche.albergo.model.Prenotazione;
 import com.ignaziopicciche.albergo.repository.ClienteRepository;
 import com.ignaziopicciche.albergo.repository.HotelRepository;
 import com.ignaziopicciche.albergo.repository.PrenotazioneRepository;
 import com.ignaziopicciche.albergo.repository.StanzaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Component
 public class PrenotazioneHelper {
@@ -24,5 +30,25 @@ public class PrenotazioneHelper {
     private StanzaRepository stanzaRepository;
 
 
+    public PrenotazioneDTO findById(Long id){
+        if(prenotazioneRepository.existsById(id)){
+            return new PrenotazioneDTO(prenotazioneRepository.findById(id).get());
+        }
+
+        throw new PrenotazioneException(PrenotazioneException.PrenotazioneExceptionCode.PRENOTAZIONE_ID_NOT_EXIST);
+    }
+
+    public List<PrenotazioneDTO> findAll(Long idHotel){
+        if(hotelRepository.existsById(idHotel)){
+            return prenotazioneRepository.findPrenotazioneByHotel_Id(idHotel).stream().map(x -> new PrenotazioneDTO(x)).collect(Collectors.toList());
+        }
+
+        throw new HotelException(HotelException.HotelExceptionCode.HOTEL_ID_NOT_EXIST);
+    }
+
+
+    public PrenotazioneDTO create(PrenotazioneDTO prenotazioneDTO){
+        if(!prenotazioneRepository.existsById(prenotazioneDTO.id) &&  )
+    }
 
 }
--- src/main/java/com/ignaziopicciche/albergo/repository/PrenotazioneRepository.java
+++ src/main/java/com/ignaziopicciche/albergo/repository/PrenotazioneRepository.java
@@ -3,5 +3,10 @@ package com.ignaziopicciche.albergo.repository;
 import com.ignaziopicciche.albergo.model.Prenotazione;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface PrenotazioneRepository extends JpaRepository<Prenotazione, Long> {
+
+    List<Prenotazione> findPrenotazioneByHotel_Id(Long idHotel);
+
 }
